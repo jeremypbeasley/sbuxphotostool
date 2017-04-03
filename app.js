2cecc33935082955d33f196f690675c35babe414
Jeremy P. Beasley <jbeasley@starbucks.com>
2017-04-03 15:47:38 -0700

    working local disk file

--- app.js
+++ app.js
@@ -10,15 +10,17 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 var http = require("http");
 var request = require('request');
-// var _ = require("lodash");
+var _ = require("lodash");
 var $ = require("jquery");
 var getJSON = require('get-json');
+var fs = require('fs');
+var util = require('util')
 
 
 // Initialize / Routing
 
 app.get("/", function(req, res) {
-  res.render("index.ejs")
+  res.render("index.ejs");
   console.log('Listening at http://localhost:2401/')
 })
 
@@ -38,6 +40,32 @@ app.get('/api/users', (req, res) => {
     });
 });
 
+function storeLocally (content) {
+  const newEntries = content.items.map(item => {
+    return {
+      id: item.id,
+      image_url: item.content,
+      name: item.name,
+      created_at: item.created_at,
+      updated_at: item.updated_at,
+      croperties: {
+        rectangle: "default",
+        square: "default",
+        circle: "default"
+      }
+    };
+  });
+  fs.readFile('public/mock.json', 'utf8', function readFileCallback(err, existingData) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const obj = JSON.parse(existingData);
+    obj.items = obj.items.concat(newEntries);
+    fs.writeFile('public/mock.json', JSON.stringify(obj))
+  });
+}
+
 app.get('/api/photos', (req, res) => {
   const options = {
     'auth': {
@@ -49,6 +77,7 @@ app.get('/api/photos', (req, res) => {
   request.get("http://app.dropmark.com/" + process.env.DROPMARK_COLLECTION_ID + ".json?key=" + process.env.DROPMARK_KEY + "&callback=?", options, function (err, result) {
     const bodyStr = result.body.slice(1, result.body.length - 1);
     const body = JSON.parse(bodyStr);
+    storeLocally(body);
     res.send(body);
   });
 });
